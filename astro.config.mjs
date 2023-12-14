8fa764fe66a09e2f5446f3b69124d3b62b957f84
Non-j <works@jirawut.com>
2023-12-14 07:47:20 +0000

    update site

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,6 @@ export default defineConfig({
 	},
 	integrations: [
 		solid(),
-		tailwind({
-			config: { applyBaseStyles: false },
-		}),
+		tailwind(),
 	],
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 const colors = require('tailwindcss/colors');
+const plugin = require('tailwindcss/plugin');
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
 	mode: 'jit',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -29,5 +31,28 @@ module.exports = {
 			typography: ['dark'],
 		},
 	},
-	plugins: [require('@tailwindcss/typography')],
+	plugins: [
+		require('@tailwindcss/typography'),
+		plugin(function ({ addUtilities }) {
+			addUtilities({
+				'.text-link': {
+					'@apply underline text-indigo-700 dark:text-indigo-200 font-medium':
+						{},
+				},
+				'.text-autoscale': {
+					'@apply text-sm sm:text-base lg:text-lg xl:text-xl': {},
+				},
+				'.w-center-column': {
+					'@apply max-w-screen-lg mx-auto px-3 md:px-8': {},
+				},
+			});
+		}),
+		plugin(function ({ addBase, theme }) {
+			addBase({
+				body: {
+					'@apply bg-yellow-50 dark:bg-slate-800 dark:text-neutral-200': {},
+				},
+			});
+		}),
+	],
 };
